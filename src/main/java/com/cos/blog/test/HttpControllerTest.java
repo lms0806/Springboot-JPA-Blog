4ec69cec0368272a1b726312d3eaab49be6d7e42
lms0806 <42136056+lms0806@users.noreply.github.com>
2021-09-28 20:25:28 +0900

    Update HttpControllerTest.java

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -26,25 +26,21 @@ public class HttpControllerTest {
 	}
 			
 	// 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
-	// http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) { // MessageConverter(스프링부트)
 		return "get 요청 :" + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	// http://localhost:8080/http/post (insert)
 	@PostMapping("/http/post")
 	public String postTest(@RequestBody Member m) { // MessageConverter(스프링부트)
 		return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	// http://localhost:8080/http/put (update)
 	@PutMapping("http/put")
 	public String putTest(@RequestBody Member m) {
 		return "put 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	// http://localhost:8080/http/delete (delete)
 	@DeleteMapping("http/delete")
 	public String deleteTest() {
 		return "delete 요청";
